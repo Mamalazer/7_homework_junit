2f748d7f5941881f5f429dcba728539c1d544c1e
Daniil Kuznetsov <ldan93@yandex.ru>
2022-11-26 15:15:01 +0300

    Create test02 in GismeteoTest

--- src/test/java/junit_practice/gismeteo/tests/GismeteoTest.java
+++ src/test/java/junit_practice/gismeteo/tests/GismeteoTest.java
@@ -4,6 +4,7 @@ import junit_practice.GeneralFrontTest;
 import junit_practice.gismeteo.pages.GeneralPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -18,6 +19,14 @@ public class GismeteoTest extends GeneralFrontTest {
     @ValueSource(strings = {"Москва", "Санкт-Петербург", "Сочи"})
     @ParameterizedTest(name = "Проверка открытия страницы с погодой в городе {0}")
     public void test01(String city) {
+        new GeneralPage()
+                .chooseCity(city)
+                .checkCityName(city);
+    }
+
+    @CsvFileSource(resources = "cities.csv")
+    @ParameterizedTest(name = "Проверка открытия страницы с погодой в городе {0}")
+    public void test02(String city) {
         new GeneralPage()
                 .chooseCity(city)
                 .checkCityName(city);
